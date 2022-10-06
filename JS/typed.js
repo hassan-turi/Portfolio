a995848b2a57e6f06dfe7e06d4a957e994881895
Hassan-Turi <hassanturi747@gmail.com>
2022-10-06 14:43:43 +0500

    Initial Commit

---
+++ JS/typed.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typing', {
+    strings:['', "Web Developer", "Back-end Developer", 'Database Designer','Front-end Developer'],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true
+})
